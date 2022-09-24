05a41814f9d0396eee65cce253b70f3cf3ba4870
Axel Maczkowiak <axel.maczkowiak@live.fr>
2022-09-24 18:21:40 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {InjectionToken, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {provideAuth, getAuth} from '@angular/fire/auth';
+import {provideDatabase, getDatabase} from '@angular/fire/database';
+import {provideFirestore, getFirestore} from '@angular/fire/firestore';
+import {AuthenticationService} from "./services/authentication.service";
+import {SETTINGS as AUTH_SETTINGS} from '@angular/fire/compat/auth';
+import {AngularFireModule} from "@angular/fire/compat";
+import {WordGridComponent} from "./components/word-grid/word-grid.component";
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, RouterModule.forRoot(routes), AngularFireModule.initializeApp(environment.firebase), provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()), WordGridComponent],
+  providers: [
+    {provide: AUTH_SETTINGS, useValue: {appVerificationDisabledForTesting: true}},
+    AuthenticationService,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mautusse-3e6f4',
+    appId: '1:290387391770:web:5f3f98c4f792fc99ae28a3',
+    storageBucket: 'mautusse-3e6f4.appspot.com',
+    apiKey: 'AIzaSyBEbNdxu3y2gOSHXLrYWPhgSTC8yXE8Qi4',
+    authDomain: 'mautusse-3e6f4.firebaseapp.com',
+    messagingSenderId: '290387391770',
+  },
   production: false
 };
 
